631f73c5ff78329ccca9d1d39bc7024ba448a65a
VladimirJG <vladimi85@gmail.com>
2023-11-22 08:15:06 +0300

    added client & schedule

--- src/main/java/org/example/dto/MeasurementDTO.java
+++ src/main/java/org/example/dto/MeasurementDTO.java
@@ -1,4 +1,31 @@
 package org.example.dto;
 
 public class MeasurementDTO {
+    private Double value;
+    private Boolean isRaining;
+    private SensorDTO sensor;
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Boolean getRaining() {
+        return isRaining;
+    }
+
+    public void setRaining(Boolean raining) {
+        isRaining = raining;
+    }
+
+    public SensorDTO getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(SensorDTO sensor) {
+        this.sensor = sensor;
+    }
 }
